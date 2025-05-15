4bf64a71cbfb6654bdf7dfacbdde6855b09d0d04
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-05-14 23:56:41 -0600

    Se agregó el proyecto al repositorio y se instaló el conector mySQK DATA

--- BaseDatos_PRACTICA2/Form1.cs
+++ BaseDatos_PRACTICA2/Form1.cs
@@ -6,5 +6,10 @@ namespace BaseDatos_PRACTICA2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
