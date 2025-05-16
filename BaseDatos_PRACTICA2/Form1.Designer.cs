dbef20f97bc3eca6f6d3c662f0125cc4361a8b7c
Ab396 <abelardoortizortiz31@gmail.com>
2025-05-16 16:23:51 -0600

    abri un nuevo form para conectarlo con base de datos

--- BaseDatos_PRACTICA2/Form1.Designer.cs
+++ BaseDatos_PRACTICA2/Form1.Designer.cs
@@ -28,20 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            Acceder = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(277, 144);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(316, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(277, 241);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(316, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(152, 151);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 20);
+            label1.TabIndex = 2;
+            label1.Text = "USUARIO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(152, 248);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 20);
+            label2.TabIndex = 3;
+            label2.Text = "CONTRASEÑA";
+            // 
+            // Acceder
+            // 
+            Acceder.BackColor = Color.PaleGreen;
+            Acceder.Location = new Point(623, 435);
+            Acceder.Name = "Acceder";
+            Acceder.Size = new Size(94, 29);
+            Acceder.TabIndex = 4;
+            Acceder.Text = "ACEPTAR";
+            Acceder.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(833, 671);
+            BackColor = Color.Teal;
+            ClientSize = new Size(781, 578);
+            Controls.Add(Acceder);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button Acceder;
     }
 }
