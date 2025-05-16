9ad43b6f7154e301a64f5a7acd3d7479a6de91a1
Ab396 <abelardoortizortiz31@gmail.com>
2025-05-16 16:43:43 -0600

    personalise  el boton de aceptar para poder acceder

--- BaseDatos_PRACTICA2/Form1.cs
+++ BaseDatos_PRACTICA2/Form1.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+
 namespace BaseDatos_PRACTICA2
 {
     public partial class Form1 : Form
@@ -11,5 +13,55 @@ namespace BaseDatos_PRACTICA2
         {
 
         }
+
+        private void Acceder_Click(object sender, EventArgs e)
+        {
+    
+            try
+            {
+                string user = textoUsuario.Text.Trim();
+                string password = textoContrasena.Text.Trim();
+
+                if (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password))
+                {
+                    labelConexion.Text = "Por favor ingresa usuario y contraseña.";
+                    return;
+                }
+
+                string server = "localhost";
+                string user1 = "root";
+                string database = "empleados";
+
+                // Cadena de conexión con los datos ingresados
+                string cadenaConexion = $"server={server};port=3307;database={database};user={user1};password={password};";
+
+                // Se crea una instancia de la conexión utilizando la cadena generada
+                MySqlConnection myConnection = new MySqlConnection(cadenaConexion);
+
+                // Se intenta abrir la conexión con la base de datos
+                myConnection.Open();
+
+                labelConexion.Text = "Conexión exitosa.";
+
+                // Ocultar la interfaz actual
+                this.Hide();
+
+                // Mostrar el form 2
+                Form2 formulario2 = new Form2(myConnection);
+                formulario2.ShowDialog();
+
+                // Al cerrar form 2 se cierra todo
+                myConnection.Close();
+                this.Close();
+            }
+            catch (Exception error)
+            {
+                // Si ocurre un error, se captura aquí
+                labelConexion.Text = "Error de conexión: " + error.Message;
+            }
+        }
+
+
     }
 }
+
--- BaseDatos_PRACTICA2/Form2.cs
+++ BaseDatos_PRACTICA2/Form2.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace BaseDatos_PRACTICA2
 {
     public partial class Form2 : Form
     {
-        public Form2()
+        private MySqlConnection conect;
+
+        public Form2(MySqlConnection conect)
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
