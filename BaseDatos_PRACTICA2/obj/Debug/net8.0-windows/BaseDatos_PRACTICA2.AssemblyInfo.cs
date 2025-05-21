394c1ac445dcd3dde76ade84870f4c80a188dd57
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-05-21 00:08:36 -0600

    Respaldo

--- BaseDatos_PRACTICA2/obj/Debug/net8.0-windows/BaseDatos_PRACTICA2.AssemblyInfo.cs
+++ BaseDatos_PRACTICA2/obj/Debug/net8.0-windows/BaseDatos_PRACTICA2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaseDatos_PRACTICA2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195c654400e7bdb48043c24caf9e17316f6d74c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25cc1f6fa3c3b824dfcc0e56337a9bd450f9a9dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaseDatos_PRACTICA2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaseDatos_PRACTICA2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
